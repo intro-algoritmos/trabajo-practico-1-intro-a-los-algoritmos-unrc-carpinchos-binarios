4552519ea3a4a510b0d09a555c4e65141e5e0987
BrisaMartoccio <163841159+BrisaMartoccio@users.noreply.github.com>
2024-05-22 13:35:32 -0300

    Actualizar CodificadorMensajes.java

--- CodificadorMensajes.java
+++ CodificadorMensajes.java
@@ -127,7 +127,17 @@ public class CodificadorMensajes
     private int[] generarCodigoEncripcion(String str) 
     {
         // TODO: Implementar este método sustituyendo la línea
-        // debajo con el código de la funcionalidad.
+        int suma = 0;
+      flor (int = 0, i<Str.length(); i ++){
+       int a = str.CharAt(i);
+      suma = suma+a;
+     }
+      int CodEncript = Suma % 99991
+       int a = 0
+     for(int i = CodEncript.lenght; (>= 0 ; i --);{
+       int a = str.CharAt(i);
+      system.out.println (a)
+     }
         return null;
     }
     
